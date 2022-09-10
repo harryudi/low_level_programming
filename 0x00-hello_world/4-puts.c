fa5c8f5049fd2d64e873468417ff714b881a39e5
harryudi <udiharrison23@gmail.com>
2022-09-10 02:33:54 -0700

    s

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function starts
+ *
+ * Return: Function closes
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
