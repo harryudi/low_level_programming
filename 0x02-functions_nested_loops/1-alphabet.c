681badc30e6f38eafa7514a4f266299e70923ca4
harryudi <udiharrison23@gmail.com>
2022-09-14 11:54:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lower case
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+
+}
