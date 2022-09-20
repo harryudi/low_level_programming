13e56bdcad9fdbafd82e312dc29fc68c24ccdfd4
harryudi <udiharrison23@gmail.com>
2022-09-20 14:36:18 -0700

    V0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -30,4 +30,8 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);                            
 
-int _atoi(char *s); 
+int _atoi(char *s);                                              
+
+                                                                 
+
+#endif /* MAIN_H */  
