2fe834bdbda8106d7de7e4e3e696f5de42207203
harryudi <udiharrison23@gmail.com>
2022-09-25 15:51:02 -0700

    e

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H                                                   
-#define MAIN_H                                                   
-                                                                 
-#include <stdio.h>  
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
